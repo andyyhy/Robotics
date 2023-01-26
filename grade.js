71178a07d0179f5f90fc74f6362fa6e8477b152f
Liz Olson <lizolson@umich.edu>
2023-01-26 08:50:05 -0500

    Update grades at time: 26-01-2023-08-40-02

--- grade.js
+++ grade.js
@@ -1,4 +1,4 @@
-//CI grader generates report at 24-01-2023-22-40-46
+//CI grader generates report at 26-01-2023-02-59-33
 grade = 
 {
   "andyyhy": {
@@ -15,54 +15,54 @@ grade =
         ]
       },
       "ROS Pub/Sub": {
-        "status": "CHECK",
-        "comments": "Passing 3 out of 3 tests"
+        "status": "DUE",
+        "comments": "Passing 0 out of 3 tests. Can get partial credit after due date"
       },
       "PathPlan_Heap": {
-        "status": "PENDING",
+        "status": "PASS",
         "comments": [
-          "0 out of 10 test cases passed"
+          "10 out of 10 test cases passed"
         ],
         "test results": {
           "Test 0": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 3341 8909 8383 6569 1443 8935 2871 4436 5798 9681 8515 2589 793 89 5164 5833 8741 316 785 3611 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 1": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 1962 8490 7327 5006 606 9166 9416 1249 9670 3933 2598 8045 5847 581 3698 5245 1722 9237 6072 1811 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 2": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 3857 4383 1245 8745 2670 1894 1148 4868 7892 8210 6067 6270 2444 5008 2414 4105 5223 1550 3559 5811 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 3": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 5159 672 1776 3955 3923 7264 3235 707 3369 985 2215 3389 9785 9584 5928 4883 5347 9493 1527 3906 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 4": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 2580 5261 9547 9302 4645 8502 2961 1107 1469 6298 1099 675 7956 8991 5107 1215 8638 3195 3266 3187 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 5": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 3789 8301 4402 498 4602 7941 2272 9183 8565 1980 3297 8 2910 5507 8952 9976 5299 4043 7098 1606 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 6": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 7357 536 472 4573 1078 8066 2930 7271 539 8109 6035 5361 5058 5319 6806 1331 4116 9847 1080 5208 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 7": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 5330 3954 209 2694 6054 675 2505 1875 6032 6917 2948 606 6137 8240 1760 2881 2298 3072 5385 2334 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 8": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 5762 4319 124 5331 2636 5011 7563 1397 5509 9754 4965 799 5011 1871 7102 3118 9439 6874 6886 1210 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 9": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 3996 2023 2849 8878 1295 2137 2233 3609 8096 8754 4876 6321 6368 8427 1191 1729 1620 8300 294 2623 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           }
         }
       },
@@ -74,23 +74,23 @@ grade =
         "test results": {
           "Test 0": {
             "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Stderr": "b'[ERROR]: Wrong result\\n'"
           },
           "Test 1": {
             "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Stderr": "b'[ERROR]: Wrong result\\n'"
           },
           "Test 2": {
             "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Stderr": "b'[ERROR]: Wrong result\\n'"
           },
           "Test 3": {
             "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Stderr": "b'[ERROR]: Wrong result\\n'"
           },
           "Test 4": {
             "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Stderr": "b'[ERROR]: Wrong result\\n'"
           }
         }
       },
