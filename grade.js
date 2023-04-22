bfef0761f71ba9590febd89ed61ea739ac403bd9
Liz Olson <lizolson@umich.edu>
2023-04-22 12:08:55 -0400

    Update grades at time: 22-04-2023-11-58-20

--- grade.js
+++ grade.js
@@ -1,5 +1,5 @@
-//CI grader generates report at 19-04-2023-18-38-32
-//Commit hash: 1f67a95
+//CI grader generates report at 22-04-2023-11-57-00
+//Commit hash: cfd6263
 grade = 
 {
   "andyyhy": {
@@ -9,383 +9,41 @@ grade =
     "course": "ROB 320/EECS 367 W23",
     "repository": "AutoRob-WN23/kineval-stencil-yuhan0204",
     "assignments": {
-      "Honor": {
-        "status": "CHECK",
-        "comments": [
-          "comment: honor code certified"
-        ]
-      },
-      "ROS Pub/Sub": {
-        "status": "PASS - on time (full credit)",
-        "comments": "Passing 3 out of 3 tests"
-      },
-      "PathPlan_Heap": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "10 out of 10 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 2": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 3": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 4": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 5": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 6": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 7": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 8": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 9": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "PathPlan_AStar": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "5 out of 5 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 2": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 3": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 4": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "Pendularm_Euler": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "2 out of 2 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "Pendularm_VelocityVerlet": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "2 out of 2 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "Pendularm_PID": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "1 out of 1 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "FK_MatrixRoutines": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "1 out of 1 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "FK_Transforms": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "8 out of 8 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 2": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 3": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 4": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 5": {
-            "Status": "Succeed",
-            "Stderr": "Only for graduate session, skip testing"
-          },
-          "Test 6": {
-            "Status": "Succeed",
-            "Stderr": "Only for graduate session, skip testing"
-          },
-          "Test 7": {
-            "Status": "Succeed",
-            "Stderr": "Only for graduate session, skip testing"
-          }
-        }
-      },
-      "FK_JointRendering": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          ""
-        ]
-      },
-      "FSMDance_Quaternion": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "1 out of 1 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "FSMDance_BaseControl": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          ""
-        ]
-      },
-      "FSMDance_SetpointControl": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "1 out of 1 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "FSMDance_FSM": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          ""
-        ]
-      },
-      "IK_Jacobian": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "2 out of 2 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "IK_JTranspose": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "3 out of 3 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 2": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "IK_JPseudoInverse": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "3 out of 3 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 2": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "MotionPlan_Collision": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "3 out of 3 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 2": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "MotionPlan_2DRRTConnect": {
-        "status": "PASS - on time (full credit)",
-        "comments": [
-          "4 out of 4 test cases passed"
-        ],
-        "test results": {
-          "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 1": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 2": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          },
-          "Test 3": {
-            "Status": "Succeed",
-            "Stderr": "b''"
-          }
-        }
-      },
-      "MotionPlan_CSpaceRRTConnect": {
-        "status": "PENDING",
-        "comments": [
-          ""
-        ]
-      },
-      "Pitch": {
-        "status": "PENDING",
-        "comments": [
-          ""
-        ]
-      },
-      "FK_BaseOffset": {
-        "status": "PENDING",
-        "comments": [
-          ""
-        ]
-      }
+      "ROS": "100.0%",
+      "PathPlan_AStar": "100.0%",
+      "PathPlan_Heap": "100.0%",
+      "Pendularm_Euler": "100.0%",
+      "Pendularm_VelocityVerlet": "100.0%",
+      "Pendularm_PID": "100.0%",
+      "FK_MatrixRoutines": "100.0%",
+      "FK_Transforms": "100.0%",
+      "FSMDance_Quaternion": "100.0%",
+      "FSMDance_SetpointControl": "100.0%",
+      "IK_Jacobian": "100.0%",
+      "IK_JTranspose": "100.0%",
+      "IK_JPseudoInverse": "100.0%",
+      "MotionPlan_Collision": "100.0%",
+      "MotionPlan_2DRRTConnect": "100.0%",
+      "FK_JointRendering": "100.0%",
+      "FK_Presentation": "100.0%",
+      "FK_NewRobot": "100.0%",
+      "FSMDance_BaseControl": "100.0%",
+      "FSM_Presentation": "100.0%",
+      "FSMDance_FSM": "100.0%",
+      "MotionPlan_CSpaceRRTConnect": "100.0%",
+      "Best Use of Robotics": "100.0%"
     },
-    "quizzes": [
-      {
-        "status": "Takehome: 1.0 Interactive: 0.7"
-      },
-      {
-        "status": "Takehome: 0.75 Interactive: 0.67"
-      },
-      {
-        "status": "Takehome: 1.0 Interactive: 1.0"
-      },
-      {
-        "status": "Takehome (Doubled): 1.5  (No Interactive)"
-      },
-      {
-        "status": "Takehome: 0.75 Interactive: 0.4"
-      },
-      {
-        "status": "Voted in the URDF showcase: 2.0  (No Takehome)"
-      },
-      {
-        "status": "Takehome: 0.9 Interactive: 1.0"
-      },
-      {
-        "status": "Voted in the FSM Dance showcase: 2.0  (No Takehome)"
-      },
-      {
-        "status": "No Quiz 9, 2 points given"
-      },
-      {
-        "status": "No Quiz 10, 2 points given"
-      },
-      {
-        "status": "No Quiz 11, 2 points given"
-      }
-    ]
+    "quizzes": {
+      "Q1": "85.0%",
+      "Q2": "83.5%",
+      "Q3": "100.0%",
+      "Q4": "75.0%",
+      "Q5": "57.49999999999999%",
+      "Q6": "100.0%",
+      "Q7": "95.0%",
+      "Q8": "100.0%",
+      "Q9": "100.0%",
+      "Q10": "100.0%"
+    }
   }
 }
